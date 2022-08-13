a0c7716b7eec0501c9fc035728e2103da601d5bf
Fouink <yyss9103@gmail.com>
2022-08-13 14:53:14 +0900

    Initial commit

---
+++ src/test/java/com/example/PersonalProject/PersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PersonalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
