156130c6e7c9b51509dddde2832a6d7aba5c6523
Fouink <yyss9103@gmail.com>
2022-08-27 03:00:47 +0900

    #1 로그아웃 기능 구현

--- src/main/java/com/example/PersonalProject/Controller/MainController.java
+++ src/main/java/com/example/PersonalProject/Controller/MainController.java
@@ -1,12 +1,28 @@
 package com.example.PersonalProject.Controller;
 
 import com.example.PersonalProject.Login.PrincipalDetails;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MainController {
+
+    @GetMapping("/api/react_test_login")
+    public ResponseEntity<?> front_login_test(@RequestParam(name = "username") String username,
+                                              @RequestParam(name = "password") String password) {
+        System.out.println(username);
+        System.out.println(password);
+        Map<String, Object> map = new HashMap<>();
+        map.put("success", "응답 성공");
+        return ResponseEntity.ok(map);
+    }
+
     @GetMapping("/")
     public String index(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         if (principalDetails != null) {
@@ -15,5 +31,14 @@ public class MainController {
             System.out.println("유저정보 없음");
         }
         return "index";
+    }
+
+    //@Authentication 잘 사라졌는지 확인 필요
+    @GetMapping("/login")
+    public ResponseEntity<?> login(){
+        Map<String, Object> map = new HashMap<>();
+        System.out.println("로그아웃 성공");
+        map.put("logout",true);
+        return ResponseEntity.ok(map);
     }
 }
--- src/main/java/com/example/PersonalProject/Login/SecurityConfig.java
+++ src/main/java/com/example/PersonalProject/Login/SecurityConfig.java
@@ -5,11 +5,14 @@ import com.example.PersonalProject.Filter.SessionAuthorizationFilter;
 import com.example.PersonalProject.User.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -20,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final SessionManager sessionManager;
     private final CorsFilter corsFilter;
     private final UserRepository userRepository;
+//    private final LogoutHandler logoutHandler;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -30,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic().disable()
                 .addFilter(new SessionAuthenticationFilter(authenticationManager(), sessionManager))
                 .authorizeRequests()
+                .antMatchers("/api/logout")
+                .access("hasRole('ROLE_USER')or hasRole('USER')or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
 //                .antMatchers("/oauth2/**").permitAll()      //이거 빼고는 authenticationManager
                 .antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
@@ -39,5 +45,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         new SessionAuthorizationFilter(authenticationManager(), sessionManager, userRepository),
                         UsernamePasswordAuthenticationFilter.class
                 );
+        http.logout()
+                .logoutUrl("/api/logout")
+                .logoutSuccessUrl("/login")
+                .deleteCookies("mySessionId")
+                .invalidateHttpSession(true);
     }
 }
