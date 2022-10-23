3761e026b772cf6b6899dbb88d08a36ebe3567ad
Fouink <yyss9103@gmail.com>
2022-10-23 14:04:15 +0900

    불필요 힘포트 제거

--- src/main/java/com/example/PersonalProject/User/ThymeleafUserController.java
+++ src/main/java/com/example/PersonalProject/User/ThymeleafUserController.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Map;
-
 @Controller
 @RequiredArgsConstructor
 public class ThymeleafUserController {
